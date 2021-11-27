374f1e56e0b7b01c9a835f25e131c0ced33566cd
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-27 23:51:11 +0900

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  const bookFetch = fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((json) => renderBooks(json));
+
+  return bookFetch;
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
